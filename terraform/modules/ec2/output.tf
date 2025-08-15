6e59bad4d9bced02bab1443461240d2ae14d9d10
Gokul M <gokulmahendiran16@gmail.com>
2025-08-15 21:47:49 +0000

    Remove terraform from main branch

--- terraform/modules/ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output "bastion_instance_id" {
-  description = "The EC2 Instance ID of the Bastion server"
-  value       = aws_instance.bastion.id
-}
-
-output "bastion_public_ip" {
-  description = "The public IP address of the Bastion server"
-  value       = aws_instance.bastion.public_ip
-}

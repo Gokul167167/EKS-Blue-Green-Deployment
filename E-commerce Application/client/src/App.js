13fbd12a1925fe480519828cf97d5b14056f4844
Gokul M <gokulmahendiran16@gmail.com>
2025-08-19 05:19:11 +0000

    updated v2 in e-commerce

--- E-commerce Application/client/src/App.js
+++ E-commerce Application/client/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Welcome to E-Commerce Website! 
+          Welcome to E-Commerce Website V2! 
         </p>
       </header>
     </div>
